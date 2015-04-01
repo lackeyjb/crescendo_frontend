475bb585eb55854b49aac2de5b6eee3f4d4749a8
Bryan Lackey <bryan.lackey@gmail.com>
2015-04-01 12:45:07 -0400

    added character spritesheet to game 1

--- client/app/scripts/controllers/game1.js
+++ client/app/scripts/controllers/game1.js
@@ -1,16 +1,16 @@
 'use strict';
 
 angular.module('crescendoApp')
-.controller('Game1Ctrl', ['$scope', function ($scope) {
+.controller('Game1Ctrl', [ function () {
 
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameview', { preload: preload, create: create,
-                                                          update: update });
+                                                                  update: update });
 
-  function preload() {
+  function preload() { // jshint ignore:line
     game.load.image('sky', 'images/sky.png');
     game.load.image('ground', 'images/platform.png');
     game.load.image('star', 'images/a-bubble.png');
-    game.load.spritesheet('dude', 'images/dude.png', 32, 48);
+    game.load.spritesheet('dude', 'images/crescendodude.png', 49.6, 68);
   }
 
   var player;
@@ -21,123 +21,126 @@ angular.module('crescendoApp')
   var score = 0;
   var scoreText;
 
-  function create() {
+  function create() { // jshint ignore:line
 
-      //  We're going to be using physics, so enable the Arcade Physics system
-      game.physics.startSystem(Phaser.Physics.ARCADE);
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-      //  A simple background for our game
-      game.add.sprite(0, 0, 'sky');
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky');
 
-      //  The platforms group contains the ground and the 2 ledges we can jump on
-      platforms = game.add.group();
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
 
-      //  We will enable physics for any object that is created in this group
-      platforms.enableBody = true;
+    //  We will enable physics for any object that is created in this group
+    platforms.enableBody = true;
 
-      // Here we create the ground.
-      var ground = platforms.create(0, game.world.height - 64, 'ground');
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
 
-      //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-      ground.scale.setTo(2, 2);
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 2);
 
-      //  This stops it from falling away when you jump on it
-      ground.body.immovable = true;
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
 
-      //  Now let's create two ledges
-      var ledge = platforms.create(400, 400, 'ground');
-      ledge.body.immovable = true;
+    //  Now let's create two ledges
+    var ledge = platforms.create(400, 400, 'ground');
+    ledge.body.immovable = true;
 
-      ledge = platforms.create(-150, 250, 'ground');
-      ledge.body.immovable = true;
+    ledge = platforms.create(-150, 250, 'ground');
+    ledge.body.immovable = true;
 
-       // The player and its settings
-      player = game.add.sprite(32, game.world.height - 150, 'dude');
+     // The player and its settings
+    player = game.add.sprite(32, game.world.height - 300, 'dude');
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(player);
 
-      //  We need to enable physics on the player
-      game.physics.arcade.enable(player);
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
 
-      //  Player physics properties. Give the little guy a slight bounce.
-      player.body.bounce.y = 0.2;
-      player.body.gravity.y = 300;
-      player.body.collideWorldBounds = true;
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                                   13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], 30, true);
+    player.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 
+                                    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], 30, true);
 
-      //  Our two animations, walking left and right.
-      player.animations.add('left', [0, 1, 2, 3], 10, true);
-      player.animations.add('right', [5, 6, 7, 8], 10, true);
+    stars = game.add.group();
 
-      stars = game.add.group();
+    stars.enableBody = true;
 
-      stars.enableBody = true;
+    //  Here we'll create 12 of them evenly spaced apart
+    for (var i = 0; i < 12; i++)
+    {
+        //  Create a star inside of the 'stars' group
+        var star = stars.create(i * 70, 0, 'star');
 
-      //  Here we'll create 12 of them evenly spaced apart
-      for (var i = 0; i < 12; i++)
-      {
-          //  Create a star inside of the 'stars' group
-          var star = stars.create(i * 70, 0, 'star');
+        //  Let gravity do its thing
+        star.body.gravity.y = 9;
 
-          //  Let gravity do its thing
-          star.body.gravity.y = 9;
+        //  This just gives each star a slightly random bounce value
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
 
-          //  This just gives each star a slightly random bounce value
-          star.body.bounce.y = 0.7 + Math.random() * 0.2;
-      }
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' 
+    });
 
-      scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' 
-      });
-
-      //  Our controls.
-      cursors = game.input.keyboard.createCursorKeys();
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
   }
 
-  function update() {
+  function update() { // jshint ignore:line
 
     //  Collide the player and the stars with the platforms
-      game.physics.arcade.collide(player, platforms);
-      game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
 
-      //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-      game.physics.arcade.overlap(player, stars, collectStar, null, this);
+    // Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
 
-      //  Reset the players velocity (movement)
-      player.body.velocity.x = 0;
+    game.physics.arcade.overlap(player, stars, collectStar, null, this); // jshint ignore:line
 
-      if (cursors.left.isDown)
-      {
-          //  Move to the left
-          player.body.velocity.x = -200;
 
-          player.animations.play('left');
-      }
-      else if (cursors.right.isDown)
-      {
-          //  Move to the right
-          player.body.velocity.x = 200;
+    // Reset the players velocity (movement)
+    player.body.velocity.x = 0;
 
-          player.animations.play('right');
-      }
-      else
-      {
-          //  Stand still
-          player.animations.stop();
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -200;
 
-          player.frame = 4;
-      }
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 200;
 
-      //  Allow the player to jump if they are touching the ground.
-      if (cursors.up.isDown && player.body.touching.down)
-      {
-          player.body.velocity.y = -320;
-      }
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 4;
+    }
+
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -320;
+    }
   }
 
   function collectStar (player, star) {
 
-        // Removes the star from the screen
-        star.kill();
+    // Removes the star from the screen
+    star.kill();
 
-        //  Add and update the score
-        score += 10;
-        scoreText.text = 'Score: ' + score;
+    //  Add and update the score
+    score += 10;
+    scoreText.text = 'Score: ' + score;
   }
 }]);
