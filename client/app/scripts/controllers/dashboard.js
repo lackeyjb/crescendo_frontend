995cdde65a5400e094cf46a84f35c65f5e8ece5c
Ric Cabrera <wawashorecords@gmail.com>
2015-04-07 11:49:41 -0400

    Styled dashboard and game

--- client/app/scripts/controllers/dashboard.js
+++ client/app/scripts/controllers/dashboard.js
@@ -42,8 +42,8 @@ function ($scope, $state, AuthService, ScoreService) {
     ]
   };
 
-  $("#myModal").on('hidden.bs.modal', function (e) {
-  $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
+  $('#myModal').on('hidden.bs.modal', function (e) {
+  $('#myModal iframe').attr('src', $('#myModal iframe').attr('src'));
   });
 
   
